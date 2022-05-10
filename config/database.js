d124649a263d0da5a2e1857aa3ecc4739619ec8c
zafrantsany <36925865+zafrantsany@users.noreply.github.com>
2022-05-10 14:38:31 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,33 +1,22 @@
+/File: config/database.js
+
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: 'default',
   connections: {
     default: {
-      connector: "mongoose",
+      connector: 'mongoose',
       settings: {
-        // uri: env('DATABASE_URI' || 'mongodb+srv://hazzstudio:Indonesi@123@hazzstudio.oq7cn.mongodb.net/backend?retryWrites=true&w=majority'),
-        // host: env("DATABASE_HOST", '127.0.0.1'),
-        // srv: env.bool("DATABASE_SRV", false),
-        // port: env.int("DATABASE_PORT", 27017),
-        // database: env("DATABASE_NAME", 'backend'),
-        // username: env("DATABASE_USERNAME", 'hazzstudio'),
-        // password: env("DATABASE_PASSWORD", 'Indonesi@123'),
-        uri: env("DATABASE_URI" || 'mongodb+srv://hazzstudio:Indonesi@123@hazzstudio.oq7cn.mongodb.net/backend?retryWrites=true&w=majority'),
-        ssl: { rejectUnauthorized: false }
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        srv: env.bool('DATABASE_SRV', false),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'api-books'),
+        username: env('DATABASE_USERNAME', null),
+        password: env('DATABASE_PASSWORD', null),
       },
       options: {
-        // authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-        // ssl: env.bool("DATABASE_SSL", false),
-        ssl: false,
-        authenticationDatabase: "",
-        useUnifiedTopology: true,
-        pool: {
-          min: 0,
-          max: 10,
-          idleTimeoutMillis: 30000,
-          createTimeoutMillis: 30000,
-          acquireTimeoutMillis: 30000
-        }
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
-});
+})
